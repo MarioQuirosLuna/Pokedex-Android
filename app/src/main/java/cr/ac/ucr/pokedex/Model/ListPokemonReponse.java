0f4897c465389c962ea73115f978a252685dae36
Mario Quirós Luna <marioquiros21@yahoo.com>
2021-06-24 00:04:17 -0600

    Colors added and code cleaned

--- app/src/main/java/cr/ac/ucr/pokedex/Model/ListPokemonReponse.java
+++ app/src/main/java/cr/ac/ucr/pokedex/Model/ListPokemonReponse.java
@@ -7,42 +7,10 @@ import com.google.gson.annotations.SerializedName;
 
 
 public class ListPokemonReponse {
-    @SerializedName("count")
-    @Expose
-    private Integer count;
-    @SerializedName("next")
-    @Expose
-    private String next;
-    @SerializedName("previous")
-    @Expose
-    private Object previous;
+
     @SerializedName("results")
     @Expose
     private List<Pokemon> results = null;
-
-    public Integer getCount() {
-        return count;
-    }
-
-    public void setCount(Integer count) {
-        this.count = count;
-    }
-
-    public String getNext() {
-        return next;
-    }
-
-    public void setNext(String next) {
-        this.next = next;
-    }
-
-    public Object getPrevious() {
-        return previous;
-    }
-
-    public void setPrevious(Object previous) {
-        this.previous = previous;
-    }
 
     public List<Pokemon> getResults() {
         return results;
